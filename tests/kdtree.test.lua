2b229a2ed0b097254b21154d7693492d5bd46f5e
Adrian Jutrowski <adrian.jutrowski@xstream.dk>
2016-02-21 09:36:40 +0100

    Add GNU GPL License

--- tests/kdtree.test.lua
+++ tests/kdtree.test.lua
@@ -1,7 +1,32 @@
+--[[
 
+#########################################################################
+#                                                                       #
+# kdtree.test.lua                                                       #
+#                                                                       #
+# Generic KDTree test class                                             #
+#                                                                       #
+# Copyright 2016 Adrian Jutrowski                                       #
+# adrian.jutrowski@gmail.com                                            #
+# 			                                                            #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License <http://www.gnu.org/licenses/> for         #
+# more details.                                                         #
+#                                                                       #
+#########################################################################
+
+--]]
 require "lunit"
 
-module( "vector.test", package.seeall, lunit.testcase )
+module( "kdtree.test", package.seeall, lunit.testcase )
 
 Point = require "Point"
 KDTree = require "kdtree"
@@ -29,12 +54,17 @@ end
 
 
 function test_nearest()
+	local tree = KDTree(points)
+	
+	assert_equal(2, #tree.root.point.coords)
 	assert_equal("table", type(points))
 	assert_equal(2, #points)
-	local tree = KDTree(points)
+	
+	tree:print();
+	
 	local test1 = tree:nearestTo(p1)
 	local test2 = tree:nearestTo(p2)
-
+	
 	assert_equal(test1, points[1])
 	assert_equal(test2, points[2])
 end
--- tests/vector.test.lua
+++ tests/vector.test.lua
@@ -1,4 +1,29 @@
+--[[
 
+#########################################################################
+#                                                                       #
+# vector.test.lua                                                       #
+#                                                                       #
+# Generic Vector test class                                             #
+#                                                                       #
+# Copyright 2016 Adrian Jutrowski                                       #
+# adrian.jutrowski@gmail.com                                            #
+# 			                                                            #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License <http://www.gnu.org/licenses/> for         #
+# more details.                                                         #
+#                                                                       #
+#########################################################################
+
+--]]
 require "lunit"
 
 module( "vector.test", package.seeall, lunit.testcase )
@@ -16,12 +41,6 @@ function setup()
   v = Vector(p1, p2)
 end
 
-function teardown()
-
-  p1 = nil 
-  p2 = nil
-  v = nil
-end
 
 function test_norm()
 	assert_equal(5, v:norm())
